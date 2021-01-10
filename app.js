22df2eecb050ebc8fc5279c9616af4106253c013
Julia Lojek <julialojek@gmail.com>
2021-01-10 13:33:07 +0100

    asynchronous working with fs

--- app.js
+++ app.js
@@ -17,12 +17,23 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    // res.writeHead(302, {});
-    // the same but separately:
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    // for every chunk of request data we fire a function passed as the second argument
+    // req.on() is a listener nad 'data' means it listens when tha data comes
+    const body = [];
+    req.on('data', chunk => {
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, err => {
+        // res.writeHead(302, {});
+        // the same but separately:
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
   }
   // console.log(req.url, req.method, req.headers);
   // process.exit(); // quits the process of running an event loop (server)
