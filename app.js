9606e9244ac3d09f39f4d53ec9951dadaa339d2f
Julia Lojek <julialojek@gmail.com>
2021-01-10 12:21:25 +0100

    redirecting and sending requests from forms

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
 
 // function rqListener(req, res) {}
 // http.createServer(rqListener);
 // or:
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>My Node App</title></head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    // res.writeHead(302, {});
+    // the same but separately:
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  // console.log(req.url, req.method, req.headers);
   // process.exit(); // quits the process of running an event loop (server)
   res.setHeader('Content-Type', 'text/html'); // later we'll do it with packages, not manually
   // writing multiple lines to the response:
