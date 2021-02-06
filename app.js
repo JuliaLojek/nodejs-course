aaa7d1a7739b45fe1c7465351d290f8761e7b015
Julia Lojek <julialojek@gmail.com>
2021-02-06 15:58:15 +0100

    middlewares for different paths

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 
 const app = express();
 // app.use() will be executed for every incoming request
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  // calling next() will end executing this function and pass to the next middleware
-  next();
-});
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
+//   console.log('In the middleware!');
+//   // calling next() will end executing this function and pass the request to the next middleware
+//   next();
+// });
+app.use('/users', (req, res, next) => {
   console.log('In another middleware!');
+  res.send('<head><title>hello</title></head><body><h1>Users</h1></body>');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<head><title>hello</title></head><body><h1>Hello!</h1></body>');
+});
 
-server.listen(3000); // makes the process running (listening)
+// not needed anymore with express:
+// const server = http.createServer(app);
+// server.listen(3000); // makes the process running (listening)
+// instead we use:
+app.listen(3000);
