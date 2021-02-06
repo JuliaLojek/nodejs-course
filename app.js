5bf31b1c7fb1ec2f4011e31a374bf7f5750bff72
Julia Lojek <julialojek@gmail.com>
2021-02-06 15:30:34 +0100

    intalling express, first middlewares

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require('http');
+const express = require('express');
 
-const { handler: routes } = require('./routes');
+const app = express();
+// app.use() will be executed for every incoming request
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  // calling next() will end executing this function and pass to the next middleware
+  next();
+});
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+});
 
-// function rqListener(req, res) {}
-// http.createServer(rqListener);
-// or:
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000); // makes the process running (listening)
