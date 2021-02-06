bec102cb4c57b759fda1275cd1769f737db9a3e4
Julia Lojek <julialojek@gmail.com>
2021-02-06 17:11:56 +0100

    parsing the request body

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 // app.use() will be executed for every incoming request
@@ -8,14 +9,27 @@ const app = express();
 //   // calling next() will end executing this function and pass the request to the next middleware
 //   next();
 // });
-app.use('/users', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<head><title>hello</title></head><body><h1>Users</h1></body>');
+
+// parsing request body and calling next() at the end:
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<head><title>hello</title></head><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form></body>'
+  );
+});
+
+// app.post() will only execute for incoming POST request, we also have app.get(), so with app.post() it will only be accessible througn post requests
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.send(
+    `<head><title>hello</title></head><body><h2>Thank you for adding '${req.body.title}' to the store!</h2></form></body>`
+  );
+  // res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<head><title>hello</title></head><body><h1>Hello!</h1></body>');
+  res.send('<head><title>hello</title></head><body><h1>Hello!</h1><a href="/add-product">add product</a></body>');
 });
 
 // not needed anymore with express:
